dc3aad264ab9917c0ecaff0af68beb38d9dcb45d
RaghavKaushal03 <kaushal.raghav13@gmail.com>
2023-01-07 11:55:27 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  res.statusCode = 3000;
   const msg = 'Hello Node from Raghav Kaushal!\n'
   res.end(msg);
 });
